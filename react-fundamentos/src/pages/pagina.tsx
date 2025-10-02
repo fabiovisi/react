a49ad58901412a50f620cde219b47092266b7408
fabio.vsilva <fabio.vsilva@dataprev.gov.br>
2025-10-02 15:53:39 -0300

    Add button styles and update imports in global styles

--- react-fundamentos/src/pages/pagina.tsx
+++ react-fundamentos/src/pages/pagina.tsx
@@ -9,12 +9,7 @@ export default function Page() {
     return (
         <Pagina titulo="Minha Página" subtitulo="Pasta /Pages">
             <button onClick={executarAcao}
-                 className={`
-                         bg-blue-500
-                         hover:bg-blue-700
-                         text-white
-                           font-bold 
-                           py-2 px-4 rounded`}>
+                 className="botao">
                 Teste
             </button>
         </Pagina>
