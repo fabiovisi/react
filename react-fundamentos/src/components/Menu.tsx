f1bd7202182b526f2fa00caf12f8f21ea36e4e0c
fabio.vsilva <fabio.vsilva@dataprev.gov.br>
2025-10-24 08:57:57 -0300

    Add communication buttons and value display for direct and indirect communication

--- react-fundamentos/src/components/Menu.tsx
+++ react-fundamentos/src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { Icon360View, IconBraces, IconBrandJavascript, IconCalculator, IconCode, IconFileCheck, IconForms, IconFunction, IconH1, IconHome, IconNumbers, IconSitemap } from "@tabler/icons-react";
+import { Icon360View, IconArrowDownSquare, IconArrowUpSquare, IconBraces, IconBrandJavascript, IconCalculator, IconCode, IconFileCheck, IconForms, IconFunction, IconH1, IconHome, IconNumbers, IconSitemap } from "@tabler/icons-react";
 import MenuItem from "./MenuItem";
 
 export default function Menu() {
@@ -8,6 +8,10 @@ export default function Menu() {
             text-3xl w72
             p-2 gap-2
         `}>
+            <span className="text-sm text-zinc-500 pl-3 pt-4">Comunicação</span>
+            <MenuItem icone={<IconArrowDownSquare />} texto="Comunicação Direta" url="/comunicacao/direta" />
+            <MenuItem icone={<IconArrowUpSquare />} texto="Comunicação Indireta" url="/comunicacao/indireta" />
+
             <span className="text-sm text-zinc-500 pl-3 pt-4">Estado</span>
             <MenuItem icone={<IconCode />} texto="Componente sem Estado" url="/estado/sem" />
             <MenuItem icone={<IconCode />} texto="Componente com Estado" url="/estado/com" />                        
